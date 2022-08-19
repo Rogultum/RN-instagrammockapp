c0cc379f4371a35a9ea232511072368654f95316
Remzi Ogul TUM <amadeuwoles@gmail.com>
2022-08-19 20:42:37 +0300

    removed backgroundcolor

--- src/components/upper_toolbar/Uppertoolbar.style.js
+++ src/components/upper_toolbar/Uppertoolbar.style.js
@@ -4,13 +4,12 @@ export default StyleSheet.create({
     container: {
         flexDirection: 'row',
         alignItems: 'stretch',
-        backgroundColor: 'rgba(0,0,0,0.025)',
     },
     instagramtext: {
         fontWeight: 'bold',
         fontSize: 24,
         padding: 16,
         paddingRight: 80,
-        color: 'black'
+        color: 'black',
     },
 })
